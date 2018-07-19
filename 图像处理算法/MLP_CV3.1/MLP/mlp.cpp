f77d96a6bb3950cae228797afab33baa472555bb
zuwuwang <1055259415@qq.com>
2018-07-19 16:45:59 +0800

    搞清楚了predict的预测输出及形式，TODO MLP输出类别、opencv2.4下的实现

--- 图像处理算法/MLP_CV3.1/MLP/mlp.cpp
+++ 图像处理算法/MLP_CV3.1/MLP/mlp.cpp
@@ -8,7 +8,7 @@
 #include "opencv2/imgcodecs.hpp"
 #include <opencv2/highgui.hpp>
 #include <opencv2/ml.hpp>
-#include<opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
@@ -22,16 +22,16 @@ const int imgHigh = 23;
 
 int SVM_TEST()
 {
-	//训练2类数据，每类4张
-	int labels[8] = { -1, -1, -1, -1, 1, 1, 1, 1 };
-	Mat labelsMat(8, 1, CV_32SC1, labels);  //将label变成矩阵的形式
+	//训练2类数据，每类4张，指明图像的类别
+	int labels[8] = { 0, 0, 0, 0, 1, 1, 1, 1 };
+	Mat labelsMat(8, 1, CV_32SC1, labels);  //将label变成矩阵的形式，数据类型要为CV_32SC1
 
 	//设置样本宽高，由原始相机像素决定
 	int smpW = imgWidth;
 	int smpH = imgHigh;
 
 	//将样本的值逐像素赋给训练图像
-	printf("start to parpare traindata...\n");
+	cout << "start to parpare traindata...\n" << endl;
 	Mat trainMat(8, smpW * smpH, CV_32FC1);
 	for (int i = 0; i < 2; i++)
 	{
@@ -62,29 +62,32 @@ int SVM_TEST()
 	Ptr<SVM> svm = SVM::create();
 	svm->setType(SVM::C_SVC);
 	svm->setKernel(SVM::LINEAR);
-	svm->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER, 100, 1e-6));
+	svm->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER, 1000, 1e-6));
 
 	//设置训练数据 
 	Ptr<TrainData> tData = TrainData::create(trainMat, ROW_SAMPLE, labelsMat);
-	cout << "start to train svm..." << endl;;
+	cout << "start to train svm..." << endl;
 	svm->train(tData);
 	cout << "train over,save svm.xml file" << endl;;
 	svm->save("svm.xml");
 
 
-	//根据训练好的模型测试数据
-	//cout<<"load svm.xml file to test ..."<<endl;
-	//Ptr<SVM> svm = SVM::load("data.xml");
-
+	//载入训练好的模型
+	cout<<"load svm.xml file to test ..."<<endl;
+	svm = SVM::load<SVM>("svm.xml");
+	
+	//载入测试数据
 	for (int i = 0; i < 2; i++)
 	{
 		for (int j = 5; j < 6; j++)
 		{
+			//一次读入一张图像进行test
 			Mat testMat(1, smpW * smpH, CV_32FC1);
 			float * testData = testMat.ptr<float>(0);
 
 			string path = format("charData\\%d%d.png",i, j);
 			Mat img = imread(path, 0);
+
 			//Canny(img, img, 50, 100);
 
 			for (int h = 0; h < smpH; h++)
@@ -97,14 +100,13 @@ int SVM_TEST()
 				}
 			}
 
-			Mat ret(1, 1, CV_32S);
+			//预测
+			Mat outputMat(1, 1, CV_32S);
 			cout<<" SVM predict resault is "<<endl;
-			float response = svm->predict(testMat, ret);  // 根据训练好的模型上进行预测
-										 // 返回1或-1，是或者否
-			
-			cout << "response: " << response << endl;
-			float * data = ret.ptr<float>(0); //识别概率
-			cout << "data[0]: "<< data[0]<<"\n" << endl;
+			float response = svm->predict(testMat, outputMat);  // 根据训练好的模型上进行预测，返回值是一个无用的float值，忽略
+			cout << "predict函数执行返回值: " << response << "\t<0为成功，否则失败!>" << endl;
+			float * data = outputMat.ptr<float>(0); //识别结果，输出为该图像所对应的标签类别
+			cout << "预测类别为 data[0]: "<< data[0]<<"\n" << endl;
 			waitKey(10);
 		}
 	}
@@ -161,9 +163,10 @@ int ANN_TEST()
 	Ptr<ANN_MLP> bp = ANN_MLP::create();
 	
 	// 定义并设置层的大小
+	// TODO ：更改输出类别数，输出4分类结果
 	Mat layerSizes = (Mat_<int>(1, 3) << smpW * smpH, 6, 1);// 输入为图像大小
-													  // 输出层有1个节点，对应两个类，label为两类
 													  // 中间隐藏层
+													 // 输出层有1个节点，对应两个类，label为两类
 	bp->setLayerSizes(layerSizes);
 	
 	// 设置网络参数
@@ -174,7 +177,8 @@ int ANN_TEST()
 	bp->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER, 10000, /*FLT_EPSILON*/1e-6));
 
 	// 保存训练好的神经网络参数
-	bool trained = bp->train(trainMat, ROW_SAMPLE, labelsMat);
+	bool trained = bp->train(trainMat, ROW_SAMPLE, labelsMat);  //ANN_MLP类继承自StatModel模型，方法train()通过函数参数重载
+														  // bool train( InputArray samples, int layout, InputArray responses )
 	if (trained) 
 	{
 		cout << "save bp_param.xml" << endl;
@@ -183,8 +187,8 @@ int ANN_TEST()
 
 
 	//创建训练好的神经网络
-	//printf("load bp_param.xml file to predict...\n");
-	//Ptr<ANN_MLP> bp = ANN_MLP::load("bp_param.xml");   //重复定义了，训练测试只需要一个
+	cout << "load bp_param.xml file to predict...\n" << endl;
+	bp = ANN_MLP::load<ANN_MLP>("bp_param.xml");   //加载训练好的模型
 
 	//读入测试数据，方法与读入待训练数据相同
 	for (int i = 0; i < 2; i++)
@@ -192,12 +196,15 @@ int ANN_TEST()
 		for (int j = 5; j < 6; j++)  // 对05 、15两张图像进行预测
 		{
 			//因为只是测试一张图像，testMat第一个元素为待测试的图像数量
-			//这里可以仿照上面的index1定义一个索引，或者直接用count++索引
+			//这里可以按上面的index1定义一个索引，或者直接用count++索引
 			Mat testMat(1, smpW * smpH, CV_32FC1);
+			
+			// int index1 = i * 2 + j - 5;
 			float * testData = testMat.ptr<float>(0);
 
 			string path = format("charData\\%d%d.png", i, j);
 			Mat img = imread(path, 0);
+
 			//Canny(img, img, 50, 100);
 
 			for (int h = 0; h < smpH; h++)
@@ -210,13 +217,17 @@ int ANN_TEST()
 				}
 			}
 			//对于每个读入的数据，使用分类器做一个预测
-			Mat responseMat;
+			Mat outputMat;
 			cout << "MLP predict is" << endl;
-			bp->predict(testMat, responseMat);
-			float response = responseMat.ptr<float>(0)[0];
-			cout <<"response: "<< response << endl;
-			float * data = responseMat.ptr<float>(0);
-			cout << "data[0]:"<< data[0] <<"\n"<< endl;
+			float response = bp->predict(testMat, outputMat);  // 分类问题，则误差表示识别率
+											// 该方法位于statModel统计模型中，继承与Algorithm类
+										    // testMat是提取的特征，作为输入；response为一个可以选择的输出矩阵，1*n维度，n为类别；flag是一个可选标志位
+											// 输出的这个结果矩阵表示什么？表示册数图像是目标的概率
+											// 返回值是一个无用的float值，忽略
+		//	float response = responseMat.ptr<float>(0)[0]; // Mat.ptr直接得到一行的指针，这里取出第一行的第一个数，error，要着没用
+			cout << "预测函数返回值response: " << response << "\t<0为成功，否则失败!>" << endl;
+			float * data = outputMat.ptr<float>(0); //data[0]取出第一行第一个数
+			cout << "预测类别 data[0]:"<< data[0] <<"\n"<< endl;
 			waitKey(10);
 		}
 	}
@@ -249,16 +260,36 @@ int imgResize()
 
 int main()
 {
+// GPS数据读取，计算方位距离
+
+// 目标检测
+	//训练
 	// 读入所有图像，并把所有图像resize为制定大小并保存
 	imgResize();
-	// 提取HOG特征，保存HOG特征图像计算梯度直方图
-	// TODO
 
-	// 读入HOG图像进行训练和分类测试
-	// TODO
+	// 提取HOG特征，保存HOG特征图像计算梯度直方图
+	// OK
+
+	// 读入HOG图像进行训练
+	// OK
 
 	// 将resize后的图像送入SVM、MLP中分类训练，并输出检测结果
-	SVM_TEST();
-	ANN_TEST();
+	// 分类器测试
+//	SVM_TEST();  // 做背景、杆塔的分类，提取ROI区域，输入ANN中
+	ANN_TEST();  // 做姿态的分类，先做四分类，正面、侧45°、侧面、塔底
+
+  //检测
+	// 以滑窗形式抽取矩形区域图像
+	// TODOhttps://docs.opencv.org/3.1.0/d0/dce/classcv_1_1ml_1_1ANN__MLP.png
+
+	// 抽取图像区域输入到目标检测分类器中分类，识别目标，得到完备的ROI区域
+	// TODO
+
+	// 将ROI区域的HOG特征图输入到姿态检测分类器中分类，得到姿态分类结果
+	// TODO
+
+	// 根据分类结果，反馈回无人机做飞控参数调整
+	// 给出方案，模拟仿真
 	system("pause");
+	return 0;
 }
